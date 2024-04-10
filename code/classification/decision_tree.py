a00e06a2401fd419ff3f6119fee304d505680863
mamf <mamf20021381@gmail.com>
2024-04-10 16:32:52 +0330

    commenting out the plots and testing classification function for breast_cancer.csv

--- code/classification/decision_tree.py
+++ code/classification/decision_tree.py
@@ -14,7 +14,7 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-classifier = DecisionTreeClassifier()
+classifier = DecisionTreeClassifier(criterion='entropy')
 classifier.fit(X_train, y_train)
 
 y_pred = classifier.predict(X_test)
