e99fe6f8d3c49556a283d90921dc31aa148bccfb
mamf <mamf20021381@gmail.com>
2024-06-03 09:11:45 +0330

    adding dimensiality reduction part

---
+++ code/dimensiality_reduction/pca.py
@@ -0,0 +1,51 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import StandardScaler
+
+from code.tools import preprocess_data
+
+X_train, X_test, y_train, y_test = preprocess_data('../../data_sets/Wine.csv')
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+pca = PCA(n_components=2)
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+
+classifier = LogisticRegression()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+matrix = confusion_matrix(y_test, y_pred)
+accuracy_score = accuracy_score(y_test, y_pred)
+print(matrix)
+print(accuracy_score)
+
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10)
+print("Accuracy: {:.2f} %".format(accuracies.mean() * 100))
+
+# displaying plot for Train Set
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.1),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.1))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('red', 'green', 'blue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+# displaying plot for Test Set
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('red', 'green', 'blue'))(i), label=j)
+plt.title('Logistic Regression (PCA)')
+plt.xlabel('PCA 1')
+plt.ylabel('PCA 2')
+plt.legend()
+plt.show()
